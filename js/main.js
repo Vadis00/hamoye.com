6031b621af842e3c1a3f363eb0ab3a3681e92760
Vadis00 <vadym.gomeniuk@binary-studio.com>
2023-04-21 20:29:14 +0300

    Programs we offer block implemented

--- js/main.js
+++ js/main.js
@@ -1,16 +1,63 @@
-// Получаем элемент с классом "img"
+
 const closeNavigation = document.querySelector('.close-mobile-navigation');
 const hamburger = document.querySelector('.hamburger-toogle');
-// Получаем элемент с классом "one"
+
 const one = document.querySelector('.mobile-navigation-conteiner');
 
-// Добавляем обработчик события клика на изображение
+
 closeNavigation.addEventListener('click', function() {
-  // Добавляем класс "active" к элементу с классом "one"
+
   one.classList.toggle('hidde-navigation');
 });
 
 hamburger.addEventListener('click', function() {
-    // Добавляем класс "active" к элементу с классом "one"
+
     one.classList.toggle('hidde-navigation');
   });
+
+
+ const slider = document.querySelector('.slider-wrapper');
+const cards = document.querySelectorAll('.card');
+const arrowLeft = document.querySelector('.action-wrapper-left');
+const arrowRight = document.querySelector('.action-wrapper-right');
+
+let counter = 0;
+const size = cards[0].clientWidth;
+
+slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+arrowRight.addEventListener('click', () => {
+  if (counter >= cards.length - 4) return;
+  slider.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  cards[1].style.margin = '0';
+});
+
+arrowLeft.addEventListener('click', () => {
+  if (counter <= 0) return;
+  slider.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  cards[1].style.marginLeft = '20px';
+});
+
+slider.addEventListener('transitionend', () => {
+  if (cards[counter].classList.contains('last-item')) {
+    slider.style.transition = "none";
+    counter = cards.length - 5;
+    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+
+  if (cards[counter].classList.contains('first-item')) {
+    slider.style.transition = "none";
+    counter = 0;
+    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+});
+
+cards.forEach((card, i) => {
+  if (i == 0) card.classList.add('first-item');
+  if (i == cards.length - 1) card.classList.add('last-item');
+});
+
